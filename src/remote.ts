ea2606cafa8bb16ccc53f01506fe41f6e233ba99
poop <noahedwardhall@gmail.com>
2023-10-23 21:13:19 -0700

    copypasta from nirv

---
+++ src/remote.ts
@@ -0,0 +1,16 @@
+import { common } from "lib/groovy/common";
+import { GroovyTraversalSource } from "./groovy/dsl";
+
+const { traversal, DriverRemoteConnection, gremlin } = common;
+
+export const g = traversal(GroovyTraversalSource).withRemote(
+  new DriverRemoteConnection("ws://0.0.0.0:8182/gremlin")
+);
+
+// FYI: neptune doesnt support parameterizition in scripts
+// may be best to skip this and focus on something more neptune relevant
+// @see https://tinkerpop.apache.org/docs/3.7.0/reference/#gremlin-javascript-scripts
+export const client = new gremlin.driver.Client("ws://0.0.0.0:8182/gremlin", {
+  traversalSource: "g",
+  session: `${Date.now()}`, // FYI: must be a string ;)~
+});
