d89e1d9ffd7211ba7cf75ac2e65a69a4b531fbea
noah edward hall <noahedwardhall@gmail.com>
2023-11-11 13:15:18 -0800

    update traversermap to accept typeof domain object (#17)
    * update test setup
    
    * update types
    
    * update TraverserMap to return typeof domain object

--- bun/types/src/examples/airRoutes.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * recreating a few examples from practical gremlin using the air-routes dataset
- *
- * groovy api translator: https://www.gremlator.com/
- */
-export {};
-//# sourceMappingURL=airRoutes.d.ts.map
