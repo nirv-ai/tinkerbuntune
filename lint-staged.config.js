d5298588274c89b51ed82da117ef0919951f834f
poop <noahedwardhall@gmail.com>
2023-11-25 14:59:36 -0800

    updates

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,8 +2,10 @@ import micromatch from 'micromatch';
 
 export default (stagedFiles) => {
   const buildFiles = micromatch(stagedFiles, [
-    'packages/**/{build,types}',
+    '**/packages/**/{build,types}',
   ]).join(' ');
+
+  console.info('\n\n root/files', buildFiles);
 
   return buildFiles.length ? ['bun run build'] : [];
 };
--- packages/utils/lint-staged.config.js
+++ packages/utils/lint-staged.config.js
@@ -3,11 +3,13 @@ import micromatch from 'micromatch';
 export default (stagedFiles) => {
   const codeFiles = micromatch(stagedFiles, ['src/**/*.*{js,ts}']).join(' ');
 
+  console.info('\n\n utils/codeFiles', codeFiles);
   return codeFiles.length
     ? [
         `bunx eslint --fix ${codeFiles}`,
         `bun run test:ci ${codeFiles}`,
         'bun run build',
+        'git add',
       ]
     : [];
 };
