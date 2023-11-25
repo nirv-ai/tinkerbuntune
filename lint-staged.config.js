c4df4369def67a5718935482c3d2941bb9f71df2
poop <noahedwardhall@gmail.com>
2023-11-25 15:14:24 -0800

    infrastructure

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,9 +3,11 @@ import micromatch from 'micromatch';
 export default (stagedFiles) => {
   const buildFiles = micromatch(stagedFiles, [
     '**/packages/**/{build,types}',
+    '**/tsconfig*.json',
+    '**/typedoc.json',
   ]).join(' ');
 
-  console.info('\n\n root/files', buildFiles);
+  // console.info('\n\n root/files', buildFiles);
 
   return buildFiles.length ? ['bun run build'] : [];
 };
--- packages/utils/src/groovy/dsl.test.ts
+++ packages/utils/src/groovy/dsl.test.ts
@@ -3,33 +3,33 @@
  * so only specific modications are in here
  * @see remote.ts for consumption of stuff in ./dsl
  */
-import * as dsl from '#utils/groovy/dsl'
+// import * as dsl from '#utils/groovy/dsl'
 
 describe('dsl', () => {
   describe('GroovyTraversal', () => {
     describe('keys', () => {
       test.skip('is chainable', async () => {
-        expect(false).toBeTrue()
-      })
-    })
+        expect(false).toBeTrue();
+      });
+    });
 
     describe('next<T>', () => {
       test.skip('typescript returns as T', () => {
-        expect(false).toBeTrue()
-      })
-    })
+        expect(false).toBeTrue();
+      });
+    });
     describe('nextMap<T>', () => {
       test.skip('typescript returns as TraverserMap<T>', () => {
-        expect(false).toBeTrue()
-      })
-    })
-  })
+        expect(false).toBeTrue();
+      });
+    });
+  });
 
   describe('anonymous', () => {
     describe('keys', () => {
       test.skip('is chainable', () => {
-        expect(false).toBeTrue()
-      })
-    })
-  })
-})
+        expect(false).toBeTrue();
+      });
+    });
+  });
+});
